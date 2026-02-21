47be795e03395ed3329d5959d8a1607b08a0800b
Gheruha <maxgheruha@gmail.com>
2026-02-21 14:35:13 +0200

    (exams/theory): added theory and algorithm for prime_application 2.

--- exams/theory/chapter2/prime_applications/algorithm.cc
+++ exams/theory/chapter2/prime_applications/algorithm.cc
@@ -1,5 +1,7 @@
+#include <cmath>
 #include <iostream>
 
+// Finding the number of n's divisors
 int application1(int n) {
   int d = 2, nr_div = 1, p;
 
@@ -24,11 +26,40 @@ int application1(int n) {
   return nr_div;
 }
 
+// Finding the sum of n's divisors
+int application2(int n) {
+  int d = 2, p;
+  long long sum = 1;
+
+  // Prime factorization
+  while (n > 1) {
+    if (n % d == 0) {
+
+      p = 0;
+      while (n % d == 0) {
+        p++;
+        n /= d;
+      }
+      long long term =
+          ((long long)std::pow(d, p + 1) - 1) / (d - 1); // sigma function
+      sum *= term;
+    }
+    d++;
+
+    if (n > 1 && d * d > n) {
+      n = d;
+    }
+  }
+
+  return sum;
+}
+
 int main() {
   int n;
   std::cin >> n;
 
-  std::cout << "Number of divisors " << application1(n) << "\n";
+  std::cout << "Number of n's divisors:  " << application1(n) << "\n";
+  std::cout << "Sum of n's divisors: " << application2(n) << "\n";
 
   return 0;
 }
