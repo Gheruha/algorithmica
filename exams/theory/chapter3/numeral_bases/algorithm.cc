13010ccfdf18060a0eaa5c3ef52ac4f47dc9b83d
Gheruha <maxgheruha@gmail.com>
2026-02-28 16:41:49 +0200

    (exams/theory): added theory and algorithm for converting base b to base 10.

--- exams/theory/chapter3/numeral_bases/algorithm.cc
+++ exams/theory/chapter3/numeral_bases/algorithm.cc
@@ -1,7 +1,7 @@
 #include <iostream>
 
 // How to determine the bigger digit of the representation in base b of the
-// number n
+// number n in base 10
 int algorithm1(int n, int b) {
   int max = 0;
   while (n != 0) {
@@ -14,13 +14,32 @@ int algorithm1(int n, int b) {
   return max;
 }
 
+// Conversion from base b to base 10
+int algorithm2(int n, int b) {
+  int res = 0;
+  for (int i = 0; i < n; i++) {
+    int x;
+    std::cin >> x;
+    res = res * b + x;
+  }
+
+  return res;
+}
+
 int main() {
   int n, b;
+  std::cout << "Write a number in base 10 and a random base: ";
   std::cin >> n >> b;
 
   std::cout << "The biggest digit of the representation in base " << b
             << " of the number " << n << ": " << algorithm1(n, b);
 
+  std::cout
+      << "\nWrite how many digits your number will have and a random base: ";
+  std::cin >> n >> b;
+  int res = algorithm2(n, b);
 
+  std::cout << "The number you wrote in base " << b
+            << " convert to base 10 is: " << res;
   return 0;
 }
