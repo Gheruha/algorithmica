af04939efe83af74832f084ef121828bbd7be073
Gheruha <maxgheruha@gmail.com>
2026-03-08 17:31:28 +0200

    (exams/theory): implemented recursive fibonacci algorithm.

--- exams/theory/chapter4/fibonacci/algorithm.cc
+++ exams/theory/chapter4/fibonacci/algorithm.cc
@@ -1,18 +1,33 @@
-// Resource: https://en.wikipedia.org/wiki/Fibonacci_sequence
 #include <iostream>
+using namespace std;
 
-int main() {
-  int n;
-  std::cin >> n;
-  int a = 1, b = 1, c;
+void fibonacci_iter(int n) {
+  long long a = 0, b = 1;
 
-  std::cout << 1 << " ";
-  for (int i = 1; i < n; i++) {
-    c = a + b;
-    std::cout << b << " ";
+  for (int i = 0; i < n; i++) {
+    cout << a << (i + 1 < n ? ' ' : '\n');
+    long long c = a + b;
     a = b;
     b = c;
   }
+}
 
+long long fibonacci_recursive(int n) {
+  if (n < 2)
+    return n;
+  return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2);
+}
+
+int main() {
+  int n;
+  cin >> n;
+
+  cout << "O(n) iterative Fibonacci (first " << n << " terms): ";
+  fibonacci_iter(n);
+
+  if (n >= 0) {
+    cout << "O(2^n) Naive recursive Fibonacci: F(" << n
+         << ") = " << fibonacci_recursive(n) << "\n";
+  }
   return 0;
 }
