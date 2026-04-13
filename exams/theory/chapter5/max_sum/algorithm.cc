6aa27f4d1db976a0feac9498e1efac9863e2bdb4
Gheruha <maxgheruha@gmail.com>
2026-04-13 17:12:30 +0300

    (exams/theory): added theory and algorithm(Kadane) with O(n) for max sum sequence.

--- exams/theory/chapter5/max_sum/algorithm.cc
+++ exams/theory/chapter5/max_sum/algorithm.cc
@@ -44,8 +44,26 @@ void algorithm2(int a[101], int n) {
   std::cout << "Interval [" << left << " ; " << right << "]\n";
 }
 
+// Time complexity O(n) - Kadane Algorithm
+void algorithm3(int a[101], int n) {
+  int left = 0, start = 0, right = 1, S = 0, Smax = -2000000000;
+
+  for (int i = 0; i < n; i++) {
+    if (S < 0)
+      S = 0, start = i;
+
+    S += a[i];
+
+    if (S > Smax)
+      Smax = S, left = start, right = i;
+  }
+
+  std::cout << "Smax: " << Smax << "\n";
+  std::cout << "Interval [" << left << " ; " << right << "]\n";
+}
+
 int main() {
   int a[101] = {-2, 3, -89, 132, -342, 343, 123, 9, 0};
-  algorithm2(a, 9);
+  algorithm3(a, 9);
   return 0;
 }
