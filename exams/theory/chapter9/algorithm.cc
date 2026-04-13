6ae4e7848580b0ec61f2f1b908453ea4664ce589
Gheruha <maxgheruha@gmail.com>
2026-04-13 14:30:15 +0300

    (exams/theory): added theory and algorithm for pascal's triangle.

---
+++ exams/theory/chapter9/algorithm.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+  int a[31][31], n;
+  std::cin >> n;
+
+  for (int i = 0; i < n; i++) {
+    a[i][0] = a[i][i] = 1;
+    for (int j = 1; j < i; j++) {
+      a[i][j] = a[i - 1][j - 1] + a[i - 1][j];
+    }
+  }
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j <= i; j++) {
+      std::cout << a[i][j] << " ";
+    }
+    std::cout << "\n";
+  }
+
+  return 0;
+}
