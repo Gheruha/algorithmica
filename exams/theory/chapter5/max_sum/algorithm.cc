6ada1db2c9d83ffa1013a6ea2f08da8cc7b0c903
Gheruha <maxgheruha@gmail.com>
2026-04-13 16:44:11 +0300

    (exams/theory): added theory and algorithm with O(n^2) for max sum sequence.

--- exams/theory/chapter5/max_sum/algorithm.cc
+++ exams/theory/chapter5/max_sum/algorithm.cc
@@ -20,8 +20,32 @@ void algorithm1(int a[101], int n) {
   std::cout << "Interval [" << left << " ; " << right << "]\n";
 }
 
+// Time complexity O(n^2) - could be used when n is around 1000.
+void algorithm2(int a[101], int n) {
+  int SP[101];
+  SP[0] = a[0];
+
+  for (int i = 0; i < n; i++) {
+    SP[i] = SP[i - 1] + a[i];
+  }
+
+  int left = 0, right = 1, S = 0, Smax = -2000000000;
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j <= n; j++) {
+      S = SP[j] - SP[i - 1];
+
+      if (S > Smax)
+        Smax = S, left = i, right = j;
+    }
+  }
+
+  std::cout << "Smax: " << Smax << "\n";
+  std::cout << "Interval [" << left << " ; " << right << "]\n";
+}
+
 int main() {
   int a[101] = {-2, 3, -89, 132, -342, 343, 123, 9, 0};
-  algorithm1(a, 9);
+  algorithm2(a, 9);
   return 0;
 }
