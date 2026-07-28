af342015eb3d6de3ea816e4df0e00982a8135d32
Gheruha <maxgheruha@gmail.com>
2026-07-28 23:29:24 +0300

    chore: removed old contents from algorithms/

--- algorithms/console/console.hpp
+++
@@ -1,9 +0,0 @@
-/* console.h - console's header. */
-#ifndef console_h
-#define console_h
-#include <string>
-using namespace std;
-
-void selectFunction(string &option1, string &option2);
-
-#endif /* console.h */
