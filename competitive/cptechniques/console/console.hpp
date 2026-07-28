a4d0fb0e1f82625fa97f915be6557416a1e1adfc
Gheruha <maxgheruha@gmail.com>
2026-07-28 23:37:04 +0300

    chore: deleted exams/ folder

--- competitive/cptechniques/console/console.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef console_hpp
-#define console_hpp
-#include <iostream>
-#include <string>
-using namespace std;
-
-void selectFunction(string &option1, string &option2);
-
-#endif /* console_hpp */
